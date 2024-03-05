d3328fe9b246d162d4b6a11a90bdf6785f39e76d
lacerdaaRodrigo <lacerdaaa.rodrigo@gmail.com>
2024-03-04 23:39:48 -0300

    comentario

--- cypress/e2e/carrinhoCompra/carrinhoCompra.cy.js
+++ cypress/e2e/carrinhoCompra/carrinhoCompra.cy.js
@@ -1,24 +1,24 @@
-/// <reference types="cypress" />
+// /// <reference types="cypress" />
 
-describe('criar conta', () => {
-
-
-    it('fazer a venda que esta no carrinho', () => {
+// describe('criar conta', () => {
 
 
-        cy.visit('https://www.demoblaze.com/index.html');
-        //Login
-        cy.login('rpl', 'pwd123', 'Welcome rpl')
-        cy.get('#cartur').click().wait(2000);
-        cy.validarProdutoCarrinho('Samsung galaxy s6', 'Nokia lumia 1520').wait(2000)
-        // E clico em "place order"
-        cy.clicarBotaoEncomendar().wait(2000)
-        // ENTÂO vou para "place order"
-        cy.preencherEncomenda().wait(2000)
-        cy.clicarComprar()
-        cy.validarVenda()
-        cy.get('.confirm').click().wait(4000)
-        //cy.voltarHome()
+//     it('fazer a venda que esta no carrinho', () => {
 
-    })
-})
+
+//         cy.visit('https://www.demoblaze.com/index.html');
+//         //Login
+//         cy.login('rpl', 'pwd123', 'Welcome rpl')
+//         cy.get('#cartur').click().wait(2000);
+//         cy.validarProdutoCarrinho('Samsung galaxy s6', 'Nokia lumia 1520').wait(2000)
+//         // E clico em "place order"
+//         cy.clicarBotaoEncomendar().wait(2000)
+//         // ENTÂO vou para "place order"
+//         cy.preencherEncomenda().wait(2000)
+//         cy.clicarComprar()
+//         cy.validarVenda()
+//         cy.get('.confirm').click().wait(4000)
+//         //cy.voltarHome()
+
+//     })
+// })
